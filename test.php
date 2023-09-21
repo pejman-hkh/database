a3900a00dd793238b6ae6e8d80c77a704d199193
pejman <pejman.hkh@gmail.com>
2023-09-21 15:06:01 +0330

    testing project until here

--- test.php
+++ test.php
@@ -6,7 +6,16 @@ $_ENV['db'] = 'mysql';
 
 $db = new \Pejman\Database\Wrapper();
 $db->connect( (object)['host' => 'localhost', 'dbName' => 'blog', 'username' => 'root', 'password' => '12c' ] );
+//$query = $db->query("select * from posts", [])->find();
 
-$query = $db->query("select * from posts", [])->find();
 
-print_r( $query );
+$test = \App\Model\Test::sql("where 1")->paginate(1,1)->find();
+print_r( \App\Model\Test::getPaginate() );
+
+print_r( $test );
+
+/*$p = new \App\Model\Test;
+$p->title = 'test';
+$id = $p->save();
+echo $id;
+exit();*/
