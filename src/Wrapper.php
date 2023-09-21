f9bceb87ea3fe284f3804af303eb8a957d40cd8c
pejman <pejman.hkh@gmail.com>
2023-09-21 15:05:31 +0330

    add default connection in main wrapper and last inster id on it

--- src/Wrapper.php
+++ src/Wrapper.php
@@ -4,16 +4,25 @@ namespace Pejman\Database;
 class Wrapper implements \Pejman\Database\Interface\Wrapper {
 
 	private $db;
-
+	public static $database;
 	function __construct() {
 		if( $_ENV['driver'] == 'PDO' ) {
 			if( $_ENV['db'] == 'mysql' ) {
 				$this->db = new \Pejman\Database\Pdo\Mysql\Wrapper();
+				self::$database = $this->db;
+				return;
 			}
 		}
+
+		//default wrapper from pdo mysql
+		$this->db = new \Pejman\Database\Pdo\Mysql\Wrapper();
+		self::$database = $this->db;						
 	}
 
+	public static $dbName;
+
 	function connect( $config ) {
+		self::$dbName = $config->dbName;
 		return $this->db->connect( $config );
 	}
 
@@ -23,6 +32,10 @@ class Wrapper implements \Pejman\Database\Interface\Wrapper {
 
 	function setConfig( $config = [] ) {
 		return $this->db->config( $config );
+	}
+
+	function lastInsertId() {
+		return $this->db->lastInsertId();
 	}
 
 	function __destruct() {
