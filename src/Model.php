2cecdf11baebe063eb47b78bdba6fca1dd28e89e
pejman <pejman.hkh@gmail.com>
2023-09-21 15:02:36 +0330

    main model no need to implement from interface because is uniq

--- src/Model.php
+++ src/Model.php
@@ -1,5 +1,100 @@
 <?php
+namespace Pejman\Database;
 
-class Model implements \Pejman\Database\Interface\Model {
+class Model {
 
+	function __construct() {
+		$this->model = $this->newModel();
+	}
+
+	public function __set($name,$value) {
+		$a = 'set'.$name;
+		if( method_exists($this, $a ) ) {
+			return $this->$a( $name, $value );
+		}
+
+		if( @$this->data->$name ) {
+			$this->data->$name = $value; 
+		}
+
+		$this->$name = $value;
+	}
+
+	private $cacheGetter;
+	public function __get($name) {
+		$a = 'get'.$name;
+		if( @$this->cacheGetter[$a] ) {
+			return $this->cacheGetter[$a];
+		}
+
+		if( method_exists($this, $a ) ) {
+			$this->cacheGetter[$a] = $ret = $this->$a();
+			return $ret;
+		}
+
+		if( @$this->data->$name ) {
+			return $this->data->$name;
+		}
+
+		return @$this->$name;
+	}
+
+	private $columns = [];
+	private $data;
+	function setData( $obj ) {
+		$this->data = $obj;
+		foreach( $obj as $k => $v ) {
+			$this->columns[] = $k;
+		}
+	}
+
+	public static function newModel() {
+		$class = get_called_class();
+
+		if( $_ENV['driver'] == 'PDO' ) {
+			if( $_ENV['db'] == 'mysql' ) {
+				return new \Pejman\Database\Pdo\Mysql\Model( $class );
+			}
+		}
+
+		//default model from pdo mysql
+		return new \Pejman\Database\Pdo\Mysql\Model( $class );
+	}
+
+	public static $models = [];
+	public static function modelInstance() {
+		$class = get_called_class();
+		if( ! @self::$models[ $class ] )
+			self::$models[ $class ] = self::newModel();
+
+		return self::$models[ $class ];
+	}
+
+	public static function sql( $sql, $bind = [] ) {
+		return self::modelInstance()->sql( $sql, $bind );
+	}
+
+	function delete() {
+		return $this->model->delete();
+	}
+
+	function save() {
+		return $this->model->save();
+	}
+
+	public static function find( $bind = [] ) {
+		return self::modelInstance()->find( $bind );
+	}
+
+	public static function field( $fields ) {
+		return self::modelInstance()->field( $bind );
+	}
+
+	public static function where( $a, $b = '', $c = '' ) {
+		return self::modelInstance()->where( $bind );
+	}
+
+	public static function getPaginate() {
+		return self::modelInstance()->getPaginate();
+	}
 }
