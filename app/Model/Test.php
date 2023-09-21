01bbd2eac24c9bcd56b9fa2ec3ac1735721b4063
pejman <pejman.hkh@gmail.com>
2023-09-21 23:30:31 +0330

    change table to test for tests

--- app/Model/Test.php
+++ app/Model/Test.php
@@ -1,5 +1,14 @@
 <?php
 namespace App\Model;
 class Test extends \Pejman\Database\Model {
-	public static $table = 'posts';
+	public static $table = 'test';
+
+	function getTestGetter() {
+		return 'getter '.$this->id;
+	}
+
+	function getTest() {
+		//if you want change some field you should use ->data inside it because you changed it with getter
+		return $this->data->test.' '.$this->id;
+	}
 }
