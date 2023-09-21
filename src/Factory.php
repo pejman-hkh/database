fe7ac0eabd014a1f831604acc366e69801c8cbfd
pejman <pejman.hkh@gmail.com>
2023-09-21 23:30:54 +0330

    change factory to static

--- src/Factory.php
+++ src/Factory.php
@@ -1,7 +1,7 @@
 <?php
 namespace Pejman\Database;
 class Factory {
-	function init() {
+	public static function init() {
 		if( $_ENV['driver'] == 'PDO' ) {
 			if( $_ENV['db'] == 'mysql' ) {
 				return new \Pejman\Database\Pdo\Mysql\Wrapper();
