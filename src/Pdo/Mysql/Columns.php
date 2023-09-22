a16b4c63e4fdc58cab71cc2a9065448331e4222d
pejman <pejman.hkh@gmail.com>
2023-09-22 21:16:16 +0330

    many bug fixed in columns class

--- src/Pdo/Mysql/Columns.php
+++ src/Pdo/Mysql/Columns.php
@@ -2,17 +2,17 @@
 namespace Pejman\Database\PDO\Mysql;
 class Columns {
 	public static $columns = [];
-	public static function get( $table ) {
-		if( self::$columns ) return self::$columns;
+	public static function init( $connection, $table ) {
+		if( @self::$columns[ $table ] ) return self::$columns[ $table ];
 
-		$getcolumns = $this->db->query("SELECT COLUMN_NAME, DATA_TYPE FROM INFORMATION_SCHEMA.COLUMNS WHERE table_name = '".$table."' and table_schema = '".\Pejman\Database\Wrapper::$dbName."'  ")->find();
+		$getcolumns = $connection->query("SELECT COLUMN_NAME, DATA_TYPE FROM INFORMATION_SCHEMA.COLUMNS WHERE table_name = '".$table."' and table_schema = '".\Pejman\Database\Wrapper::$dbName."'  ")->find();
 		$columns = [];
 		foreach( $getcolumns as $v ) {
-			$columns[] = $v['COLUMN_NAME'];
-			$columnsType[] = [ $v['COLUMN_NAME'],  $v['DATA_TYPE'] ];
+			$columns[] = $v->COLUMN_NAME;
+			$columnsType[] = [ $v->COLUMN_NAME,  $v->DATA_TYPE ];
 		}
 
-		self::$columns = [ $columns, $columnsType ]
-		return self::$columns;	
+		self::$columns[ $table ] = [ $columns, $columnsType ];
+		return self::$columns[ $table ];	
 	}	
 }
