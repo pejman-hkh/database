08727e3f6e2f06b1031d1313144c3b83170ba6dc
pejman <pejman.hkh@gmail.com>
2023-09-22 21:13:38 +0330

    delete factoryModel and add factoryModelResult instead

--- src/FactoryModelResult.php
+++ src/FactoryModelResult.php
@@ -1,14 +1,14 @@
 <?php
 namespace Pejman\Database;
-class FactoryModel {
+class FactoryModelResult {
 
 	public static function init( $class ) {
 		if( $_ENV['driver'] == 'PDO' ) {
 			if( $_ENV['db'] == 'mysql' ) {
-				return new \Pejman\Database\Pdo\Mysql\Model( $class );
+				return new \Pejman\Database\Pdo\Mysql\ModelResult( $class );
 			}
 		}
 		//default model from pdo mysql
-		return new \Pejman\Database\Pdo\Mysql\Model( $class );		
+		return new \Pejman\Database\Pdo\Mysql\ModelResult( $class );		
 	}	
 }
