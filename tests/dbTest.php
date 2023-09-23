169aa579123c610bd12c51374c242c47748a2482
pejman <pejman.hkh@gmail.com>
2023-09-23 14:23:22 +0330

    make test1 table for better test

--- tests/dbTest.php
+++ tests/dbTest.php
@@ -44,13 +44,37 @@ final class dbTest extends TestCase
             ALTER TABLE `test`
               MODIFY `id` int NOT NULL AUTO_INCREMENT;
             COMMIT;");
-
-            $query = $this->db->query("SHOW TABLES LIKE 'test'")->find()[0];
-            $this->assertSame( 'test', $query->{'Tables_in_blog (test)'} );
+        
         } catch( Exception $e ) {
-            $query = $this->db->query("SHOW TABLES LIKE 'test'")->find()[0];
-            $this->assertSame( 'test', $query->{'Tables_in_blog (test)'} );
         }
+        $query = $this->db->query("SHOW TABLES LIKE 'test'")->find()[0];
+        $this->assertSame( 'test', $query->{'Tables_in_blog (test)'} );
+    }
+
+
+    public function test1CreateTable() {
+        try {
+
+            $query = $this->db->query("SHOW TABLES LIKE 'test1'")->find()[0];
+            $this->assertSame( null, $query );
+
+            $this->db->query("CREATE TABLE `test1` (
+            `id` int NOT NULL,
+            `title` varchar(500) COLLATE utf8mb3_persian_ci NOT NULL,
+            `testid` int NOT NULL
+            ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_persian_ci;
+
+            ALTER TABLE `test1`
+              ADD PRIMARY KEY (`id`);
+
+            ALTER TABLE `test1`
+              MODIFY `id` int NOT NULL AUTO_INCREMENT;
+            COMMIT;");
+        
+        } catch( Exception $e ) {
+        }
+        $query = $this->db->query("SHOW TABLES LIKE 'test1'")->find()[0];
+        $this->assertSame( 'test1', $query->{'Tables_in_blog (test1)'} );
     }
 
     public function testLastInsertId() {
